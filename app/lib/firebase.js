5636563a8c519bd89bcfc4daf0d5917bf8f3977d
Add_Dun <adundiya18@gmail.com>
2026-04-21 21:46:16 +0100

    Move Firebase config to env

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBIsCotDIZ1lgvxVqZX3s-iEeNYFhUPy8",
-  authDomain: "dunerbase.firebaseapp.com",
-  projectId: "dunerbase",
-  storageBucket: "dunerbase.firebasestorage.app",
-  messagingSenderId: "717516189692",
-  appId: "1:717516189692:web:1c3ba6b323e23b5f89cc79"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
